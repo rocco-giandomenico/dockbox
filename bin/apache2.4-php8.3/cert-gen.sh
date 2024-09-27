e0366017db112d177f68c3357b27660b50478a59
Rocco Giuseppe Giandomenico <r.giandomenico@gmail.com>
2024-09-27 02:40:27 +0200

    change | localhost https

--- bin/apache2.4-php8.3/cert-gen.sh
+++ bin/apache2.4-php8.3/cert-gen.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
 
-FOLDER="/etc/apache2/ssl/"
+DEF_KEYSIZE=2048
+DEF_DAYS=3650
+DEF_SIGN_SIGNATURE="sha256"
+CA_KEY_FILE="/etc/apache2/ssl/root-ca.key"
+CA_CRT_FILE="/etc/apache2/ssl/root-ca.crt"
+SUBJECT="/C=IT/ST=Rome/L=Rome/O=dockbox/OU=dockbox/CN=dockbox.org/emailAddress=ca@dockbox.org"
 
-openssl genrsa -out ${FOLDER}root-ca.key 2048
-openssl req -x509 -new -nodes -key ${FOLDER}root-ca.key -sha256 -days 3650 -out ${FOLDER}root-ca.pem \
-  -subj "/C=IT/ST=Rome/L=Rome/O=dockbox/OU=dockbox/CN=dockbox.org/emailAddress=ca@dockbox.org"
+CA_CONFIG="$( cat <<'HEREDOC'
+[req]
+distinguished_name = req_distinguished_name
 
-openssl genrsa -out ${FOLDER}dockbox.key 2048
+[req_distinguished_name]
 
-DOMAIN="dok"
+[ v3_ca ]
+basicConstraints = critical, CA:TRUE
+subjectKeyIdentifier = hash
+keyUsage = critical, digitalSignature, cRLSign, keyCertSign
+authorityKeyIdentifier = keyid:always,issuer:always
+HEREDOC
+)"
 
-openssl req -new -key ${FOLDER}dockbox.key -out ${FOLDER}dockbox.csr \
-  -subj "/C=IT/ST=Rome/L=Rome/O=dockbox/OU=dockbox/CN=localhost/emailAddress=ca@dockbox.org" \
-  -reqexts SAN \
-  -config <(printf "[req]\ndistinguished_name=req_distinguished_name\n[req_distinguished_name]\nC=IT\nST=Rome\nL=Rome\nO=dockbox\nOU=dockbox\nCN=localhost\nemailAddress=ca@dockbox.org\n[SAN]\nsubjectAltName=DNS:localhost,DNS:*.${DOMAIN}\n")
+openssl genrsa \
+  -out ${CA_KEY_FILE} \
+  ${DEF_KEYSIZE}
 
-openssl x509 -req -in ${FOLDER}dockbox.csr -CA ${FOLDER}root-ca.pem -CAkey ${FOLDER}root-ca.key -CAcreateserial -out ${FOLDER}dockbox.crt -days 365 -sha256
+openssl req \
+  -new \
+  -x509 \
+  -nodes \
+  -${DEF_SIGN_SIGNATURE} \
+  -days ${DEF_DAYS} \
+  -key ${CA_KEY_FILE} \
+  -subj ${SUBJECT} \
+  -extensions v3_ca \
+  -config <(echo \"${CA_CONFIG}\") \
+  -out ${CA_CRT_FILE}
